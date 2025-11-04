06e3a56d02483187e404eeaac72f65f896fa87e1
Edward Eleazar <eleazaredward@gmail.com>
2025-11-04 23:52:54 +0100

    chore(added some basic implementation)

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 #!/usr/bin/env node
 
 import { welcomeMessage } from "./helper.js";
+import readline from "readline";
 
 class NumberGuessingGame {
   constructor(){
-
+    this.readline = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    this.highScores = new Map()
+    this.stats = {
+      gamesPlayed: 0,
+      gamesWon: 0,
+      totalAttempts: 0,
+      bestTime: null
+    };
   } 
 
   async startgame(){
     try {
       console.clear();
       welcomeMessage();
+
+      let play = true;
+      while(play){
+        
+      }
     } catch (error) {
       console.log(error)
     }
